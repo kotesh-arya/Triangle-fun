1e98f93dfb7da9d38eb8a5624e971b132cfbf4b4
kotesh_Mudila <koteswarraomudila@gmail.com>
2021-09-30 11:46:54 +0530

    updated msg for negative and null inputs for area app

--- area.js
+++ area.js
@@ -6,15 +6,21 @@ const message = document.querySelector("#message-div");
 
 function productOfSides(a, b) {
     const sideProduct = a * b;
-    return sideProduct;
+return sideProduct;
 }
-
-function calculateArea() {
-    const sideProduct = productOfSides(Number(sides[0].value), Number(sides[1].value));
-    const area = 1/2 *(sideProduct);
-    message.innerText = `The area of the triangle  with the entered values is ${area}`;
+function calculateArea(){
     
+      if(sides>0){
+        const sideProduct= productOfSides(sides[0].value,sides[1].value);
+        const area = 1/2 *  (sideProduct);
+        message.innerText = `The area of triangle with the entered values is ${area} Cm-Square`;
+        
+      }else{
+        message.innerText = `enter valid values!`;
 
-}
+      }
+    }
+
+
 
 checkButton.addEventListener("click", calculateArea);
